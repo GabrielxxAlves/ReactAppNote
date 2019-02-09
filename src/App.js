f044e66f7f7c41bf25c8ad5b7b543495b5b220c1
Gabriel Alves <gp_alves@outlook.com>
2019-02-09 20:56:00 -0300

    version 3

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
 
   render() {
     return (
-      <div>
+      <div className='app'>
           <HeaderHome/>
           <div className='conteudo'>
             <div class="ui placeholder segment">
